021b33764650e2fc12a7e9d89f1a8261e2ea951a
Anna Edgar <aedgar@tangcapital.com>
2019-11-08 15:53:53 -0800

    start on fixing deleted files

---
+++ src/pages/getStarted/GetStarted.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './GetStarted.module.css'
+
+const GetStarted = () => {
+  return (
+    <div className={styles.wrapper}>
+
+    </div>
+  )
+}
+
+export default GetStarted
